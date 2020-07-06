70e2633cbee5c51009a4cd374b8b94691df0fc97
ryan <root@LAPTOP-0QBJNRA9.localdomain>
2020-07-06 18:19:39 -0400

    driver file created and fcfs template

---
+++ driver.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+#include "fcfs.h"
+
+int main(int argc, char** argv){
+
+    test();
+
+    return EXIT_SUCCESS;
+}
---
+++ fcfs.h
@@ -0,0 +1,14 @@
+#ifndef __fcfs_h
+#define __fcfs_h
+
+#include <iostream>
+#include <vector>
+
+void test();
+
+void test(){
+    std::cout << "void test" << std::endl;
+}
+
+
+#endif
