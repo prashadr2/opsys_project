b41f6e1fb4c1c9d201d4ebb7f99a2b741c9f5c88
ryan <prashadr2@gmail.com>
2020-07-13 17:05:32 -0400

    sjf start

--- sjf.h
+++ sjf.h
@@ -5,10 +5,34 @@
 #include <iostream>
 #include <vector>
 
-void sjftest();
+void sjf(std::ofstream& outfile, const std::vector<Process>& p, const int tcs, const int alpha);
+void printqueue(std::list<Process>& printer);
+void printqueue(std::list<Process>& printer){ //THIS FUNCTION PRINTS A NEWLINE CHAR!!!
+  std::cout << "[Q";
+  if(printer.size() == 0){
+      std::cout << " <empty>]" << std::endl;
+      return;
+  }
+  for(std::list<Process>::iterator z = printer.begin(); z != printer.end(); z++) {
+      std::cout << ' ' << z->getname();
+#ifdef DEBUG_MODE
+    std::cout << "-->waittime: " << z->getcurrentwait() << ", ";
+#endif
+  }
+  std::cout << "]" << std::endl;
+}
 
-void sjftest(){
-    std::cout << "void sjf test" << std::endl;
+void sjf(std::ofstream& outfile, const std::vector<Process>& p, const int tcs, const int alpha){
+    outfile << "Algorithm FCFS\n"; //write to file test... working
+    int t = 0; //time
+    std::list<Process> unarrived;
+    std::list<Process> ready; //first positon in ready queue IS THE PROCESS CURRENTLY IN THE CPU
+    std::list<Process> waiting;
+    Process* incpu = NULL; //needs to be a pointer so we can dynammically call the process deep copy constructor
+    auto compname = [](Process p1, Process p2) -> bool {return p1.getname() < p2.getname();};
+    auto comparrival = [](Process p1, Process p2) -> bool {if(p1.getarrivaltime() == p2.getarrivaltime()) return p1.getname() < p2.getname(); else return p1.getarrivaltime() < p2.getarrivaltime();};
+    auto compcurwait = [](Process p1, Process p2) -> bool {if(p1.getcurrentwait() == p2.getcurrentwait()) return p1.getname() < p2.getname(); else return p1.getcurrentwait() < p2.getcurrentwait();};
+    
 }
 
 
