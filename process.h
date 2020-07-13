39a3e684fc496fb73ab5ab9ba868db153848b677
emartsolf <emilymartsolf@comcast.net>
2020-07-13 18:07:02 -0400

    Update process.h

--- process.h
+++ process.h
@@ -15,6 +15,18 @@ class Process{
         bursts = zbursts;
         cpu_time = zcpu_time;
         io_time = zio_time;
+        tau = -1;
+        previousBurst = -1;
+    }
+
+    Process(std::string zname, int zarrival_time, int zbursts, std::list<int> zcpu_time, std::list<int> zio_time, int ztau, int zpreviousBurst){
+        name = zname;
+        arrival_time = zarrival_time;
+        bursts = zbursts;
+        cpu_time = zcpu_time;
+        io_time = zio_time;
+        tau = ztau;
+        previousBurst = zpreviousBurst;
     }
 
     Process(const Process& p){ //shallow copies cause big worries later on
@@ -27,6 +39,8 @@ class Process{
         for(auto const& data : p.getiotime()) cpyio.push_back(data);
         this->cpu_time = cpycpu;
         this->io_time = cpyio;
+        this->tau = p.getTau();
+        this->previousBurst = p.getPreviousBurst();
     }
     
     std::string getname() const {return name;}
@@ -46,6 +60,11 @@ class Process{
     int getcurrentruntime() {return cpu_time.front();}
     void movenextruntime() {cpu_time.pop_front();}
     void decreaseruntime(int d) {*(cpu_time.begin()) -= d;}
+    void setPreviousBurst(int b) {previousBurst = b;}
+    int getPreviousBurst() const {return previousBurst;}
+
+    int getTau() const {return tau;}
+    void recalculateTau(int alpha) {tau = (alpha * previousBurst) + ((1 - alpha) * tau);}
 
     private:
     std::string name;
@@ -53,7 +72,8 @@ class Process{
     int bursts;
     std::list<int> cpu_time;
     std::list<int> io_time; //if the last element is -2, that means no io burst time is here
-    bool waiting;
+    int tau;
+    int previousBurst;
 };
 
 #endif
